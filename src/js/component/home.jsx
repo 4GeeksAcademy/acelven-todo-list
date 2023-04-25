e9a51ac4deb7e45dc8926b86f9eb87a98f354e5e
Acelven <124097190+Acelven@users.noreply.github.com>
2023-04-25 00:53:29 +0000

    TodoList preview

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,15 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+import ReactDOM from "react-dom";
+import "./styles.css";
+import TodoList from './todo.jsx'
+  
+var destination = document.querySelector("#app");
+  
+ReactDOM.render(
+    <div>
+        <TodoList/>
+    </div>,
+    destination
+);
 
 export default Home;
